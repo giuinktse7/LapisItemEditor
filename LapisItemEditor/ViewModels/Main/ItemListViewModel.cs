1a7519cdb8f6f03e39df5509e7000cc4215db7f1
Jonathan Krän <jonathan.kran1@gmail.com>
2022-07-25 13:48:56 +0200

    Add search (#3)

--- LapisItemEditor/ViewModels/Main/ItemListViewModel.cs
+++ LapisItemEditor/ViewModels/Main/ItemListViewModel.cs
@@ -6,27 +6,110 @@ using System;
 using DynamicData;
 using DynamicData.Binding;
 using System.Reactive.Linq;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Windows.Input;
 
 namespace LapisItemEditor.ViewModels
+
 {
+
+
     public sealed class ItemListViewModel : ViewModelBase
     {
         private SourceList<ItemModel> _items;
         private ReadOnlyObservableCollection<ItemModel> _observableItems;
 
+        private bool searchForServerId;
+        private string searchQuery;
         public ReactiveCommand<ItemModel, ItemModel> ItemTypeSelected { get; }
+
+        public ICommand Search { get; }
+
+        public string SearchQuery { get => searchQuery; set => this.RaiseAndSetIfChanged(ref searchQuery, value); }
+        public bool SearchForServerId { get => searchForServerId; set => this.RaiseAndSetIfChanged(ref searchForServerId, value); }
+
+
+        public class ItemModelComparer<T> : IEqualityComparer<ItemModel>
+        {
+            Func<ItemModel, T> f;
+            public ItemModelComparer(Func<ItemModel, T> f)
+            {
+                this.f = f;
+            }
+
+            public bool Equals(ItemModel? x, ItemModel? y)
+            {
+                return f(x).Equals(f(y));
+            }
+
+            public int GetHashCode([DisallowNull] ItemModel obj)
+            {
+                return (int)obj.ClientId;
+            }
+        }
+
+        public int getIndex(uint id, bool clientId = true)
+        {
+            var item = new ItemModel();
+            Func<ItemModel, uint> f;
+            if (clientId)
+            {
+                f = (a) => a.ClientId;
+                item.ClientId = id;
+            }
+            else
+            {
+                f = (a) => a.ServerId;
+                item.ServerId = id;
+            }
+
+            return Items.AsObservableList().Items.IndexOf(item, new ItemModelComparer<uint>(f));
+        }
+
 
         public ItemListViewModel()
         {
             ItemTypeSelected = ReactiveCommand.Create<ItemModel, ItemModel>(itemModel => { return itemModel; });
             Items = new SourceList<ItemModel>();
 
+            var searchFilter = this.WhenValueChanged(x => x.SearchQuery)
+            .Select(SearchNamePredicate);
+
+            var sorter = this.WhenValueChanged(x => x.SearchQuery)
+            .Select(SortComparer);
+
             var loader = Items
                 .Connect()
-                .Sort(SortExpressionComparer<ItemModel>.Ascending(x => x.ServerId))
+                .Filter(searchFilter)
+                .Sort(sorter)
                 .ObserveOn(RxApp.MainThreadScheduler)
                 .Bind(out _observableItems)
                 .DisposeMany().Subscribe();
+        }
+
+        private Func<ItemModel, bool> SearchNamePredicate(string query)
+        {
+            bool isInt = int.TryParse(query, out var result);
+            if (isInt || query == null || query.Length < 3)
+            {
+                return model => true;
+            }
+
+            return model => FuzzySharp.Fuzz.Ratio(query, model.Name) >= 0.8;
+        }
+
+        private IComparer<ItemModel> SortComparer(string query)
+        {
+            bool isInt = int.TryParse(query, out var result);
+            if (isInt || query == null || query.Length < 3)
+            {
+                return SortExpressionComparer<ItemModel>.Ascending(x => x.ServerId);
+            }
+            else
+            {
+                return SortExpressionComparer<ItemModel>.Descending(model => FuzzySharp.Fuzz.Ratio(query, model.Name));
+            }
         }
 
         public SourceList<ItemModel> Items
--- LapisItemEditor/Views/Main/ItemListView.axaml.cs
+++ LapisItemEditor/Views/Main/ItemListView.axaml.cs
@@ -1,21 +1,83 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
+using Avalonia.Threading;
 using LapisItemEditor.ViewModels;
 using static LapisItemEditor.ViewModels.ItemListViewModel;
 
 namespace LapisItemEditor.Views
 {
-    public partial class ItemListView : ReactiveUserControl<WelcomeViewModel>
+
+    public partial class ItemListView : ReactiveUserControl<ItemListViewModel>
     {
+
+        public static Action Debounce(Action f, int milliseconds = 300)
+        {
+            var last = 0;
+            return () =>
+            {
+                var current = Interlocked.Increment(ref last);
+
+                Task.Delay(milliseconds).ContinueWith(task =>
+                {
+                    if (current == last)
+                    {
+                        Dispatcher.UIThread.Post(() => f());
+                    }
+                    task.Dispose();
+                });
+            };
+        }
+        private ScrollViewer scrollView;
+        private Action debouncedSearch;
+        private Action debouncedWrapper;
+
+        public TextBox SearchBox => this.FindControl<TextBox>("search_box");
+
+
         public ItemListView()
         {
             InitializeComponent();
+
+            scrollView = this.FindControl<ScrollViewer>("scroller");
+            debouncedWrapper = Debounce(search, 300);
+        }
+
+        private void search()
+        {
+            if (ViewModel == null)
+            {
+                return;
+            }
+
+            ViewModel.SearchQuery = SearchBox.Text;
+
+            if (int.TryParse(SearchBox.Text, out var result))
+            {
+                bool useClientId = !ViewModel.SearchForServerId;
+                var index = ViewModel.getIndex((uint)result, useClientId);
+
+                if (index != -1)
+                {
+                    scrollView.Offset = new Avalonia.Point(0, index * 51);
+                }
+            }
+            else
+            {
+                scrollView.Offset = new Avalonia.Point(0, 0);
+            }
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+
+            var a = SearchBox;
+
         }
 
         public void OnSelectTemplateKey(object sender, SelectTemplateEventArgs e)
@@ -28,5 +90,13 @@ namespace LapisItemEditor.Views
 
             e.TemplateKey = "defaultKey";
         }
+
+
+
+        private void searchBoxKeyUp(object sender, KeyEventArgs e)
+        {
+            debouncedWrapper();
+        }
+
     }
 }
